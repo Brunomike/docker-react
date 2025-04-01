302dcd1fb4044be4f765b083081607462d641afa
Michael Bruno <55556263+Brunomike@users.noreply.github.com>
2025-04-01 23:03:31 +0300

    changed app text

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React test  tweets
+          I was changed on the feature branch
         </a>
       </header>
     </div>
